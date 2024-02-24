b83a2e5aa84caf6083048ee9687bade76f72bd06
Devi krishna <devikrishna2345@gmail.com>
2024-02-24 10:26:26 +0530

    login,register

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -19,37 +19,47 @@ module.exports = {
         res.render('auth/user/register')
     },
     userRegister:async (req,res)=>{
-         console.log(req.body);
-        // const {username,firstName,lastName,email,password,confirmPassword}=req.body;
+        //  console.log(req.body);
+         const errors = validationResult(req);
+         console.log(errors);
+         if (!errors.isEmpty()) {
+           req.flash(
+             "error",
+             errors.array().map((err) => err.msg)
+           );
+           // return res.status(422).json({ errors: errors.array() });
+           return res.redirect("/register");
+         }
+        const {username,firstName,lastName,email,password,confirmPassword}=req.body;
         
-        // const existingUser=await User.findOne({email});
+        const existingUser=await User.findOne({email});
 
-        // if(existingUser){
-        //     req.flash("error","Email already in use");
-        //     return res.redirect("/register")
-        // }
+        if(existingUser){
+            req.flash("error","Email already in use");
+            return res.redirect("/register")
+        }
 
-        // if(password !== confirmPassword){
-        //     req.flash("error","Password do not match");
-        //     return res.redirect("/register");
-        // }
+        if(password !== confirmPassword){
+            req.flash("error","Password do not match");
+            return res.redirect("/register");
+        }
 
-        // const user=new User({
-        //     username,
-        //     firstName,
-        //     lastName,
-        //     email,
-        //     password,
-        // })
+        const user=new User({
+            username,
+            firstName,
+            lastName,
+            email,
+            password,
+        })
 
-        // let savedUser = await user.save();
+        let savedUser = await user.save();
 
-        // if (!savedUser){
-        //     req.flash("error","User Registration Unsuccessfull");
-        //     return res.redirect("/register");
-        // }else{
-            
-        // }
+        if (!savedUser){
+            req.flash("error","User Registration Unsuccessfull");
+            return res.redirect("/register");
+        }else{
+            req.session.verifyToken = savedUser._id;
+        }
     },
     getVerifyOtp: async (req, res) => {
         res.render('auth/user/verifyOtp')
